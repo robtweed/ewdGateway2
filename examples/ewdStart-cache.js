a040f3219ca9341af61ae8657a3b2abdaf4b6039
Rob Tweed <rtweed@mgateway.com>
2013-02-11 16:44:35 +0000

    Updated Cache Startup example

--- examples/ewdStart-cache.js
+++ examples/ewdStart-cache.js
@@ -1,8 +1,8 @@
-var ewd = require('./ewdGateway2');
+var ewd = require('../lib/ewdGateway2');
 
 var params = {
-      poolSize: 3,
-      httpPort: 8081,
+      poolSize: 2,
+      httpPort: 8080,
 	  https: {
 	    enabled: false,
 	    keyPath: "c:\\node\\ssl\\ssl.key",
@@ -17,19 +17,20 @@ var params = {
       },
       database: {
         type: 'cache',
-        nodePath: "c:\\Program Files\\nodejs\\cache",
+        nodePath: "c:\\Program Files (x86)\\nodejs\\cache",
         path:"c:\\InterSystems\\Cache\\Mgr",
         username: "_SYSTEM",
         password: "SYS",
         namespace: "USER",
-        outputFilePath:"c:\\temp\\",
+        outputFilePath:"c:\\node\\",
       },
-	  childProcessPath: __dirname + '/ewdQWorker.js',
+	  childProcessPath: '../lib/ewdQWorker.js',
       traceLevel: 2,
       silentStart: false,
-      webServerRootPath: 'c:\\Inetpub\\wwwroot',
+      webServerRootPath: 'c:\\node\\www',
       logTo: 'console',
       logFile: 'c:\\node\\ewdLog.txt',
+	  logHTTP: false,
       monitorInterval: 30000,
       ewdQPath: './ewdQ',
       management: {
