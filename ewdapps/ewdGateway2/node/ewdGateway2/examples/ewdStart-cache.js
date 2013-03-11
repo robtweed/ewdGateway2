610ec4b899798bdc926d08dad4e16bffe0e94a18
Rob Tweed <rtweed@mgateway.com>
2013-03-11 17:05:14 +0000

    ewdGateway2 Management EWD Application

---
+++ ewdapps/ewdGateway2/node/ewdGateway2/examples/ewdStart-cache.js
@@ -0,0 +1,49 @@
+var ewd = require('../lib/ewdGateway2');
+
+var params = {
+      poolSize: 2,
+      httpPort: 8080,
+	  https: {
+	    enabled: false,
+	    keyPath: "c:\\node\\ssl\\ssl.key",
+	    certificatePath: "c:\\node\\ssl\\ssl.crt",
+      },
+      ewdPath: '/ewd/',
+      webSockets: {
+        enabled: true,
+        path: '/ewdWebSocket/',
+        host: '127.0.0.1',
+        socketIoPath: 'c:\\node\\node_modules\\socket.io\\lib\\socket.io'
+      },
+      database: {
+        type: 'cache',
+        nodePath: "c:\\Program Files (x86)\\nodejs\\cache",
+        path:"c:\\InterSystems\\Cache\\Mgr",
+        username: "_SYSTEM",
+        password: "SYS",
+        namespace: "USER",
+        outputFilePath:"c:\\node\\",
+      },
+	  childProcessPath: '../lib/ewdQWorker.js',
+      traceLevel: 2,
+      silentStart: false,
+      webServerRootPath: 'c:\\node\\www',
+      logTo: 'console',
+      logFile: 'c:\\node\\ewdLog.txt',
+	  logHTTP: true,
+      monitorInterval: 30000,
+      ewdQPath: './ewdQ',
+      management: {
+        password: 'keepThisSecret!'
+     }
+};
+
+ewd.start(params,function(gateway) {
+  //console.log("version = " + gateway.version());
+  
+   gateway.messageHandler.testit = function(request) {
+    console.log("*!*!*!*!*! Processing the testit message " + request.message + "; User's EWD token:" + request.token);
+	gateway.sendSocketMsg({token: request.token, type: "alert", message: "Node.js handled your request"});
+  };
+  
+});
